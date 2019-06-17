c8e533a858afa1eb1cba2917554a5432f8849447
Jorge Calapina <jorgecalapina@gmail.com>
2019-06-17 21:35:49 +1200

    Creation of classes for the model

---
+++ NetPotentialTestASPNET/NetPotentialTestASPNET/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace NetPotentialTestASPNET.Models
+{
+    public class UserModel
+    {
+        public int[] SelectedTeaIds { get; set; }
+        public IEnumerable<SelectListItem> TeaList { get; set; }
+    }
+}
